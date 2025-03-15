5fd57be2ea6ecb3c334c0ac4c9ac6716502ac18e
Aditya Bhalerao <adityab7968@gmail.com>
2025-03-15 22:17:09 +0530

    Add Prototype pattern

---
+++ creational/prototype/cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/abha1erao/design-patterns-in-go/creational/prototype"
+)
+
+func main() {
+	// Original DockerContainer
+	baseContainer := prototype.NewDockerContainer()
+	baseContainer.Describe()
+	fmt.Println()
+
+	// Clone 1: For Backend Service
+	backendContainer := baseContainer.Clone().(*prototype.DockerContainer)
+	backendContainer.CPU = 4
+	backendContainer.Ports = append(backendContainer.Ports, 3000)
+	backendContainer.EnvVars["SERVICE"] = "backend"
+
+	backendContainer.Describe()
+	fmt.Println()
+
+	// Clone 2: For Database Service
+	dbContainer := baseContainer.Clone().(*prototype.DockerContainer)
+	dbContainer.Image = "postgres:latest"
+	dbContainer.CPU = 8
+	dbContainer.Memory = 8192
+	dbContainer.EnvVars["DB_USER"] = "admin"
+
+	dbContainer.Describe()
+}
